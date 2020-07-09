403d663745af429647133ed8e482fb8311aeb363
hmbashar <hmbashar@gmail.com>
2020-07-10 01:43:44 +0600

    prograss bar

--- inc/template.php
+++ inc/template.php
@@ -1,43 +1,118 @@
 <?php 
  // Don't call the file directly
 if ( !defined( 'ABSPATH' ) ) exit;
-get_header();
-?>
+	get_header();
 
-
-		<?php 
 		
-	$order = wc_get_order( 783 );
-echo $order->get_status();
+	$order = wc_get_order( 987 );
+	$order_data = $order->get_data(); // The Order data
 
-
-		?>
+?>
 
 
 	<div class="alishop-tracking-container">
 		<div class="alishop-tracking">
-
 			<div class="alishop-tracking-title">
-				<h2>Project Tracker</h2>
+				<h2>Order Tracker</h2>
 			</div>
 
+
 			<div class="alishop-tracking-porgressbar">
-				<div class="alishop-tracking-progress" style="width: 50%;"></div>
+				<div class="alishop-tracking-progress" style="width: 40%;"></div>				
+				  <div class="alishop_wc_traking_steps">
+				  	<!--Single Step-->
+				    <div class="alishop_wc_traking_step">				    	
+				    	<div class="alishop-wc-traking-img">
+				    		<img src="<?php echo CB_WC_TRACKER_URL . 'img/place-order.png'; ?>" title="Order" alt="delivery">
+				    	</div>
+				    	<div class="alishop-wc-traking-round active"></div>
+				    	
+				    </div><!--/ Single Step-->
+
+					<!--Single Step-->
+				    <div class="alishop_wc_traking_step">				    	
+				    	<div class="alishop-wc-traking-img">
+				    		<img src="<?php echo CB_WC_TRACKER_URL . 'img/payment.png'; ?>" title="Payment Pending" alt="delivery">
+				    	</div>
+				    	<div class="alishop-wc-traking-round active ali-wc-payment"></div>
+				    	
+				    </div>
+				    <!--/ Single Step-->
+					<!--Single Step-->
+				    <div class="alishop_wc_traking_step">				    	
+				    	<div class="alishop-wc-traking-img">
+				    		<img src="<?php echo CB_WC_TRACKER_URL . 'img/hold.png'; ?>" title="On Hold" alt="delivery">
+				    	</div>
+				    	<div class="alishop-wc-traking-round active ali-wc-hold"></div>
+				    	
+				    </div>
+				    <!--/ Single Step-->
+					<!--Single Step-->
+				    <div class="alishop_wc_traking_step">				    	
+				    	<div class="alishop-wc-traking-img">
+				    		<img src="<?php echo CB_WC_TRACKER_URL . 'img/processing.png'; ?>" title="Processing" alt="delivery">
+				    	</div>
+				    	<div class="alishop-wc-traking-round active ali-wc-processing"></div>
+				    	
+				    </div>
+				    <!--/ Single Step-->
+					<!--Single Step-->
+				    <div class="alishop_wc_traking_step">				    	
+				    	<div class="alishop-wc-traking-img">
+				    		<img src="<?php echo CB_WC_TRACKER_URL . 'img/delivery.png'; ?>" title="Shipping" alt="delivery">
+				    	</div>
+				    	<div class="alishop-wc-traking-round active ali-wc-shipping"></div>
+				    	
+				    </div>
+				    <!--/ Single Step-->
+					<!--Single Step-->
+				    <div class="alishop_wc_traking_step">				    	
+				    	<div class="alishop-wc-traking-img">
+				    		<img src="<?php echo CB_WC_TRACKER_URL . 'img/delivered.png'; ?>" title="delivered" alt="delivery">
+				    	</div>
+				    	<div class="alishop-wc-traking-round active ali-wc-delivered"></div>
+				    	
+				    </div>
+				    <!--/ Single Step-->
+
+				  </div>	
+
 			</div>
 
 			<div class="alishop-tracking-list">
 				<table>
 					<tr>
-						<td>Project ID:</td>
-						<td>1234567</td>
+						<td>Order ID:</td>
+						<td><?php echo esc_html($order->get_id()); ?></td>
 					</tr>
 					<tr>
-						<td>Project name:</td>
-						<td>Web Content Management Integration </td>
+						<td>Order Date</td>
+						<td><?php echo esc_html($order->post->post_title); ?></td>						
 					</tr>
 					<tr>
-						<td>Project notes:</td>
-						<td>New process to be included</td>
+						<td>Order Status</td>
+						<td><?php echo esc_html(ucwords($order->get_status())); ?></td>						
+					</tr>
+					<tr>
+						<td>Last Update</td>
+						<td><?php echo esc_html($order_data['date_modified']->date('d-M-y @ h:i:s A')); ?></td>
+					</tr>
+
+					<?php 
+						$i = 0;
+						foreach ($order->get_items() as $item_key => $item ):
+							$i++;
+					?>
+						<tr>
+							<td>Product name: (<?php echo esc_html($i); ?>)</td>					
+							<td><a href="<?php the_permalink($item->get_product_id());?>"> <?php echo esc_html($item->get_name()); ?></a></td>						
+						</tr>
+						
+					<?php endforeach; ?>
+
+					<tr>
+						<td>Customer Name:</td>
+						<td><?php echo esc_html($order_data['billing']['first_name']); ?> <?php echo esc_html($order_data['billing']['last_name']); ?></td>
 					</tr>
 					<tr>
 						<td>Customer Notes:</td>
--- js/ajax-active.js
+++ js/ajax-active.js
@@ -1,26 +1,5 @@
 (function($) {
 
 
-	$('.cb-domain-search-form form').on('submit', function() {
-		var CbDomainCheckInput = $('.cb-domain-check input#Search').val();
-		var CBDomainSearchNonce = $('.cb-domain-check input#Search').attr('data-nonce');
-		$.ajax({
-			type: 'post',
-			url:CbDomainSearch.ajaxurl,
-			data:{
-				action: 'cb_domain_check_result',
-				domain_name:CbDomainCheckInput,
-				data_nonce:CBDomainSearchNonce,
-			},
-			beforeSend:function() {
-				$('.cb_domain_check_loader').addClass('cb-domain-lds-ellipsis');
-			},
-			success: function(data) {
-				$('.cb_domain_check_loader').removeClass('cb-domain-lds-ellipsis');
-				$('.cb-domain-search-result').html(data);
-
-			}
-		});		
-		return false
-	});
 })(jQuery);
+
