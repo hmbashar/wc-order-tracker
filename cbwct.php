5299c5da652f2b1a51b10b939aeeb6e7b60eb9cb
hmbashar <hmbashar@gmail.com>
2023-02-27 14:37:53 +0600

    version updated

--- cbwct.php
+++ cbwct.php
@@ -4,7 +4,7 @@
 	Plugin URI: https://wordpress.org/plugins/alishop-wc-order-tracker
 	Description: Your customer can track his/her order on your WooCommerce website easily, woocommerce order tracking system with ajax
 	Author: Md Abul Bashar
-	Version: 1.1
+	Version: 1.2.1
 	Author URI: https://facebook.com/hmbashar
 	Text Domain: cbwct
 
